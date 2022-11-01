49ad77bdfe10df9d3fdb46ab80a59a55d90f6656
evelynhelena <evelynhelena781@gmail.com>
2022-10-31 21:19:28 -0300

    conectando com o back

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+interface HomeProps {
+  count: number;
+}
+export default function Home({count}:HomeProps) {
   return (
-    <h1>Ola NLW</h1>
+    <h1>{count}</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const reponse = await fetch('http://localhost:3333/pools/count');
+  const data = await reponse.json();
+
+  console.log(data) 
+
+  return {
+    props: {
+      count: data.count
+    }
+  }
 }
